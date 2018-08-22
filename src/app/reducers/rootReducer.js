aa70d852e6dcbdcccd2b248db2d503c1064d7961
HenryITS <henryitsolutions@gmail.com>
2018-08-22 18:08:15 +0200

    end of 12

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -2,11 +2,17 @@ import { combineReducers } from "redux";
 import { reducer as FormReducer } from "redux-form";
 import testReducer from "../../features/testarea/testReducer";
 import eventReducer from "../../features/event/eventReducer";
+import modalsReducer from "../../features/modals/modalReducer";
+import authReducer from "../../features/auth/authReducer";
+import asyncReducer from "../../features/async/asyncReducer";
 
 const rootReducer = combineReducers({
   test: testReducer,
   events: eventReducer,
-  form: FormReducer
+  form: FormReducer,
+  modals: modalsReducer,
+  auth: authReducer,
+  async: asyncReducer
 });
 
 export default rootReducer;
--- src/features/nav/navBar/NavBar.jsx
+++ src/features/nav/navBar/NavBar.jsx
@@ -1,29 +1,38 @@
-import React, { Component } from 'react';
-import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink, Link, withRouter } from 'react-router-dom';
-import SignedOutMenu from '../Menus/SignedOutMenu';
-import SignedInMenu from '../Menus/SignedInMenu';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "../Menus/SignedOutMenu";
+import SignedInMenu from "../Menus/SignedInMenu";
+import { openModal } from "../../modals/modalActions";
+import { logout } from "../../auth/authActions";
+
+const actions = {
+  openModal,
+  logout
+};
+
+const mapState = state => ({
+  auth: state.auth
+});
 
 class NavBar extends Component {
-  state = {
-    authenticated: false
+  handleSignIn = () => {
+    this.props.openModal("LoginModal");
   };
 
-  handleSignIn = () => {
-    this.setState({
-      authenticated: true
-    });
+  handleRegister = () => {
+    this.props.openModal("RegisterModal");
   };
 
   handleSignOut = () => {
-    this.setState({
-      authenticated: false
-    });
-    this.props.history.push('/')
+    this.props.logout();
+    this.props.history.push("/");
   };
 
   render() {
-    const { authenticated } = this.state;
+    const { auth } = this.props;
+    const authenticated = auth.authenticated;
     return (
       <Menu inverted fixed="top">
         <Container>
@@ -33,10 +42,11 @@ class NavBar extends Component {
           </Menu.Item>
           <Menu.Item as={NavLink} to="/events" name="Events" />
           <Menu.Item as={NavLink} to="/test" name="Test" />
-          {authenticated &&
-            <Menu.Item as={NavLink} to="/people" name="People" />}
+          {authenticated && (
+            <Menu.Item as={NavLink} to="/people" name="People" />
+          )}
 
-          {authenticated &&
+          {authenticated && (
             <Menu.Item>
               <Button
                 as={Link}
@@ -46,16 +56,28 @@ class NavBar extends Component {
                 inverted
                 content="Create Event"
               />
-            </Menu.Item>}
+            </Menu.Item>
+          )}
           {authenticated ? (
-            <SignedInMenu signOut={this.handleSignOut} />
+            <SignedInMenu
+              currentUser={auth.currentUser}
+              signOut={this.handleSignOut}
+            />
           ) : (
-              <SignedOutMenu signIn={this.handleSignIn} />
-            )}
+            <SignedOutMenu
+              register={this.handleRegister}
+              signIn={this.handleSignIn}
+            />
+          )}
         </Container>
       </Menu>
     );
   }
 }
 
-export default withRouter(NavBar);
+export default withRouter(
+  connect(
+    mapState,
+    actions
+  )(NavBar)
+);
